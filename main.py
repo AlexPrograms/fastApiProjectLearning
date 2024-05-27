b34eff7834c06cfce82a5031b93a448eafd4758e
AlexPrograms <olexiy2004@gmail.com>
2024-05-27 13:33:14 +0200

    Basics part 4 (all requests using db)

--- main.py
+++ main.py
@@ -1,79 +1,95 @@
 from typing import List
 
-
-from sqlmodel import create_engine, SQLModel, Session
+from sqlmodel import create_engine, SQLModel, Session, select
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 
-from schemas import load_db, save_db, WorkerInput, WorkerOutput, JobInput, JobOutput, Worker
+from schemas import WorkerInput, WorkerOutput, JobInput, JobOutput, Worker, Job
 
-app = FastAPI(title="Rectuitment Info")
-#json db
-db = load_db()
-#database creation (change from sqlite to Postgresql or whatever)
+app = FastAPI(title="Recruitment Info")
+
+# database creation (change from sqlite to PostgreSQL or whatever)
 engine = create_engine(
-    "sqlite:///rectuitment.db",
+    "sqlite:///recruitment.db",
     connect_args={"check_same_thread": False},
     echo=True,
 )
 
-
-#start function
+# start function
 @app.on_event("startup")
 def startup():
     SQLModel.metadata.create_all(engine)
 
+# creating session and using dependency injection to use it in every request
+# instead of return I use generator function and yield which could act like an iterator
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 @app.get("/api/workers")
-async def get_workers(age:int|None=None, profession:str|None=None) -> List:
-    result = db
-    if age is not None:
-        result = [worker for worker in result if worker.age == age]
-    if profession is not None:
-        result = [worker for worker in result if worker.profession.lower() == profession.lower()]
-    return result
-@app.get("/api/workers/{id}")
-async def worker_by_id(id:int):
-    result = [worker for worker in db if worker.id == id]
-    if result is not None:
-        return result
+async def get_workers(age: int = None, profession: str = None,
+                      session: Session = Depends(get_session)) -> List[WorkerOutput]:
+    query = select(Worker)
+    if age:
+        query = query.where(Worker.age == age)
+    if profession:
+        query = query.where(Worker.profession == profession)
+    return session.exec(query).all()
+
+@app.get("/api/workers/{id}", response_model=Worker)
+async def worker_by_id(id: int, session: Session = Depends(get_session)) -> Worker:
+    worker = session.get(Worker, id)
+    if worker:
+        return worker
     else:
-        raise HTTPException(status_code=404, detail="No Worker Found")
+        raise HTTPException(status_code=404, detail=f"Worker with id {id} is not found")
 
 @app.post("/api/workers/", response_model=Worker)
-async def add_worker(worker_input: WorkerInput) -> Worker:
-    #creating transaction to execute a bunch of operations together or don't execute anything
-    with Session(engine) as session:
-        new_worker = Worker.from_orm(worker_input)
-        session.add(new_worker)
-        session.commit()
-        session.refresh(new_worker)
-        return new_worker
-
+async def add_worker(worker_input: WorkerInput,
+                     session: Session = Depends(get_session)) -> Worker:
+    new_worker = Worker.from_orm(worker_input)
+    session.add(new_worker)
+    session.commit()
+    session.refresh(new_worker)
+    return new_worker
 
 @app.delete("/api/workers/{id}", status_code=204)
-async def delete_worker(id:int)->None:
-    matches = [worker for worker in db if worker.id == id]
-    if matches is not None:
-        worker = matches[0]
-        db.remove(worker)
-        save_db(db)
+async def delete_worker(id: int, session: Session = Depends(get_session)) -> None:
+    worker = session.get(Worker, id)
+    if worker:
+        session.delete(worker)
+        session.commit()
     else:
-        raise HTTPException(status_code=404,
-                            detail=f"No Worker with id={id}Found")
+        raise HTTPException(status_code=404, detail=f"No Worker with id={id}")
 
-@app.put("/api/workers/{id}", response_model=WorkerOutput)
-async def update_worker(id:int, new_data: WorkerInput) -> WorkerOutput:
-    matches = [worker for worker in db if worker.id == id]
-    if matches is not None:
-        worker = matches[0]
-        worker.age = new_data.age
+@app.put("/api/workers/{id}", response_model=Worker)
+async def update_worker(id: int, new_data: WorkerInput,
+                        session: Session = Depends(get_session)) -> Worker:
+    worker = session.get(Worker, id)
+    if worker is not None:
         worker.profession = new_data.profession
+        worker.age = new_data.age
         worker.name = new_data.name
-        save_db(db)
+        session.commit()
         return worker
     else:
         raise HTTPException(status_code=404, detail=f"No Worker with id={id}")
+
+@app.post("/api/jobs", response_model=Job)
+async def add_job(worker_ids: List[int], job_input: JobInput,
+                  session: Session = Depends(get_session)) -> Job:
+    employees = []
+    for worker_id in worker_ids:
+        worker = session.get(Worker, worker_id)
+        if worker:
+            employees.append(worker)
+
+    new_job = Job.from_orm(job_input, update={"worker_ids":worker_ids})
+    new_job.workers = employees
+    session.add(new_job)
+    session.commit()
+    session.refresh(new_job)
+    return new_job
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
--- schemas.py
+++ schemas.py
@@ -1,24 +1,20 @@
-import json
 from datetime import date
+from typing import List, Optional
 
 from sqlmodel import SQLModel, Field, Relationship
-#from pydantic import BaseModel
-
 
 class JobInput(SQLModel):
     title: str
     salary: float
     description: str
     location: str
-    start_date: date | None
-    end_date: date | None
-
-
+    start_date: Optional[date] = None
+    end_date: Optional[date] = None
 
 class WorkerInput(SQLModel):
     name: str
-    age: int | None
-    profession: str | None
+    age: Optional[int] = None
+    profession: Optional[str] = None
 
     class Config:
         schema_extra = {
@@ -29,23 +25,18 @@ class WorkerInput(SQLModel):
             }
         }
 
-class Worker(WorkerInput, table = True):
-    id: int | None = Field(primary_key=True, default=None)
+class Worker(WorkerInput, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    job_id: Optional[int] = Field(default=None, foreign_key="job.id")
+    job: Optional["Job"] = Relationship(back_populates="workers")
 
 class WorkerOutput(WorkerInput):
     id: int
 
 class JobOutput(JobInput):
     id: int
-    employees: list[WorkerOutput]=[]
+    employees: List[WorkerOutput] = []
 
-
-
-def load_db()-> list[WorkerOutput]:
-    """Load a list of workers from a JSON file"""
-    with open('workers.json') as file:
-        return [WorkerOutput.parse_obj(obj) for obj in json.load(file)]
-
-def save_db(workers: list[WorkerOutput]):
-    with open('workers.json', 'w') as file:
-        json.dump([worker.dict() for worker in workers], file, indent=4)
+class Job(JobInput, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    workers: List[Worker] = Relationship(back_populates="job")
